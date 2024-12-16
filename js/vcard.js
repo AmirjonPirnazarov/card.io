7965c6fc69cb94638e4c4b64aff78b1d4ed84a12
AmirjonPirnazarov <86454687+AmirjonPirnazarov@users.noreply.github.com>
2024-12-16 23:24:01 +0500

    Add files via upload

---
+++ js/vcard.js
@@ -0,0 +1,22 @@
+function downloadVCard() {
+    const vCard = `BEGIN:VCARD
+VERSION:3.0
+FN:Amir Pirnazarov
+TITLE:Head of Digital DMA @Yagonchi, Co-Founder
+TEL:+992920090909
+EMAIL:pirnazarova988@gmail.com
+ADR:;;Tajikistan, Khujand, 735700;;;;
+END:VCARD`;
+
+    const blob = new Blob([vCard], { type: 'text/vcard' });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'amir_pirnazarov.vcf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+    
+    alert('Скачивание визитки началось!');
+}
